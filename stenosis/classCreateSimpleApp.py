7560aa924e2cd0b209d4c48393d1fcd0ca92171f
Fredrik Eikeland Fossan <fredrik.e.fossan@ntnu.no>
2018-12-11 08:42:16 +0100

    added new viscous and geometrical case

--- stenosis/classCreateSimpleApp.py
+++ stenosis/classCreateSimpleApp.py
@@ -44,14 +44,13 @@ class SetupApp:
         #                       'deltaP_viscous':deltaP_viscous,                     [Pa]
         #                        'deltaP_viscous_pouseille':deltaP_viscous_pouseille [Pa]
         #===================================================
-
-        self.solutionDict, self.solutionDict_space = self.load_data()
+        self.solutionDict, self.solutionDict_space, self.solutionDict_mu, self.solutionDict_space_mu, self.solutionDict_geom, self.solutionDict_space_geom, self.solutionDict_geom_mu, self.solutionDict_space_geom_mu = self.load_data()
         #===================================================
         # create sample-points along longitudinal (z) axis
         #===================================================
         #self.samplepointsLongitudinal = np.arange(-20, 71, 0.1)#np.arange(-20, 70, 1.)
         #self.samplepointsLongitudinal = np.arange(-100, 150.1, 0.1)
-        self.samplepointsLongitudinal = np.arange(-20, 120.1, 0.1)
+        self.samplepointsLongitudinal = np.arange(-20, 140.1, 0.1)
         #===================================================
         # retrieve datoa along the z-axis
         #===================================================
@@ -104,14 +103,14 @@ class SetupApp:
 
         self.plot_pressure.circle(x="x", y="P", source=self.source_pos, size=12, color='black', fill_alpha=1)
         self.plot_pressure.legend.background_fill_alpha = 0.
-        self.plot_velocity_pos = Figure(plot_height=450, plot_width=600, title="radial velocity profile at given position",
+        self.plot_velocity_pos = Figure(plot_height=450, plot_width=600, title="velocity profile at given position",
                                         x_axis_label="Uz [m/s]", y_axis_label="r [mm]",
                                         tools="crosshair,pan,reset,resize,save,wheel_zoom",
                                         x_range=[-0.2, 3.0], y_range=[0, 1.6])
         
         self.plot_velocity_pos.line('y', 'x', source=self.source, color='black', line_alpha=1, line_width=2)
 
-        self.plot_pressure_pos = Figure(plot_height=450, plot_width=600, title="radial pressure profile at given position",
+        self.plot_pressure_pos = Figure(plot_height=450, plot_width=600, title="pressure profile at given position",
                                         x_axis_label="P [mmHg]", y_axis_label="r [mm]" ,
                                         tools="crosshair,pan,reset,resize,save,wheel_zoom",
                                         x_range=[np.min(P)*1.9, np.max(P)*1.1], y_range=[0, 1.6])
@@ -120,8 +119,11 @@ class SetupApp:
         
          
 
-        self.positionSlider = Slider(title="position", value=-20, start=-20, end=120, step=0.1)
+        self.positionSlider = Slider(title="position", value=-20, start=-20, end=140, step=0.1)
+        self.viscositySelect = Select(title="select viscosity mP", value="3.5", options=["3.5", "1.75"])
+        self.stenosisLengthSelect = Select(title="select stenosis length [cm]", value="1", options=["1", "2"])
         self.viscousSelect = Select(title="show viscous part", value="False", options=["True", "False"])
+        self.velocityComponentSelect = Select(title="select velocity component", value="Uz", options=["Uz", "Ur"])
         #self.viscousPouseilleSelect = Select(title="show viscous part (pouseille)", value="False", options=["True", "False"])
         self.convectiveSelect = Select(title="show convective part", value="False", options=["True", "False"])
         #self.convectiveFlatSelect = Select(title="show convective part (flat)", value="False", options=["True", "False"])
@@ -132,7 +134,7 @@ class SetupApp:
         #self.imageSelect = Select(title="select image", value="red", options=["red", "black"])
 
 
-        self.Widgetlist = [self.positionSlider, self.viscousSelect, self.convectiveSelect, self.oneDSelect]
+        self.Widgetlist = [self.positionSlider, self.viscositySelect, self.stenosisLengthSelect, self.velocityComponentSelect, self.viscousSelect, self.convectiveSelect, self.oneDSelect]
         
     def update_data(self, attrname, old, new):
 
@@ -140,27 +142,48 @@ class SetupApp:
         # conversion factors
         #===================================================
         m_to_mm = 1e3
+        mm_to_m = 1e-3
         Pa_to_mmHg = 1./133.32
         
+        
         xPos = self.positionSlider.value
+        viscosity = self.viscositySelect.value
+        stenosisLength = self.stenosisLengthSelect.value
+        velocityComponent = self.velocityComponentSelect.value
         
         viscous = self.viscousSelect.value
         convective = self.convectiveSelect.value
         oneD = self.oneDSelect.value
-
+        
+        if viscosity == "3.5":
+            if stenosisLength == "1":
+                solutionDict = self.solutionDict
+                solutionDict_space = self.solutionDict_space
+            else:
+                solutionDict = self.solutionDict_geom
+                solutionDict_space = self.solutionDict_space_geom
+            mu = 3.5*1e-3
+        else:
+            if stenosisLength == "1":
+                solutionDict = self.solutionDict_mu
+                solutionDict_space = self.solutionDict_space_mu
+            else:
+                solutionDict = self.solutionDict_geom_mu
+                solutionDict_space = self.solutionDict_space_geom_mu
+            mu = 0.5*3.5*1e-3
         #===================================================
         # retrieve datoa along the z-axis
         #===================================================
-        X = self.solutionDict_space['x']*m_to_mm
-        R = self.solutionDict_space['r']*m_to_mm
-        P = self.solutionDict_space['P']*Pa_to_mmHg
-        P_tot = self.solutionDict_space['P_tot']*Pa_to_mmHg
-        U = self.solutionDict_space['U']
+        X = solutionDict_space['x']*m_to_mm
+        R = solutionDict_space['r']*m_to_mm
+        P = solutionDict_space['P']*Pa_to_mmHg
+        P_tot = solutionDict_space['P_tot']*Pa_to_mmHg
+        U = solutionDict_space['U']
         
-        P_convective = P[0] - self.solutionDict_space['deltaP_convective']*Pa_to_mmHg
-        P_convective_flat = P[0] - self.solutionDict_space['deltaP_convective_flat']*Pa_to_mmHg
-        P_viscous = P[0] - self.solutionDict_space['deltaP_viscous']*Pa_to_mmHg
-        P_viscous_pouseille = P[0] - self.solutionDict_space['deltaP_viscous_pouseille']*Pa_to_mmHg
+        P_convective = P[0] - solutionDict_space['deltaP_convective']*Pa_to_mmHg
+        P_convective_flat = P[0] - solutionDict_space['deltaP_convective_flat']*Pa_to_mmHg
+        P_viscous = P[0] - solutionDict_space['deltaP_viscous']*Pa_to_mmHg
+        P_viscous_pouseille = P[0] - solutionDict_space['deltaP_viscous_pouseille']*Pa_to_mmHg
         
         P_oneD = P_convective + P_viscous - P[0]
         P_oneD_conventional = P_convective_flat + P_viscous_pouseille - P[0]
@@ -168,21 +191,34 @@ class SetupApp:
         #===================================================
         # retrieve data at a certain position along the z axis
         #===================================================
-        r = self.solutionDict[xPos]['r']*m_to_mm
-        uz = self.solutionDict[xPos]['uz']
-        ur = self.solutionDict[xPos]['ur']
-        p = self.solutionDict[xPos]['P']*Pa_to_mmHg
+        r = solutionDict[xPos]['r']*m_to_mm
+        uz = solutionDict[xPos]['uz']
+        ur = solutionDict[xPos]['ur']
+        p = solutionDict[xPos]['P']*Pa_to_mmHg
         
         indx = np.where(np.abs(self.samplepointsLongitudinal - xPos) < 0.0001)
         
         xPos = X[indx]
         rPos = R[indx]
         pPos = P[indx]
+        uPos = U[indx]
+        rho = 1050
+
+        Re = rho*uPos*2*(rPos*mm_to_m)/mu
 
         #self.source = ColumnDataSource(data=dict(x=r, y=uz, P=p,))
         #self.source_pos = ColumnDataSource(data=dict(x=xPos, r=rPos, P=pPos,))
-        
-        self.source.data = dict(x=r, y=uz, P=p)
+        if velocityComponent == "Uz":
+            self.source.data = dict(x=r, y=uz, P=p)
+            self.plot_velocity_pos.xaxis.axis_label = "Uz [m/s]"
+            self.plot_velocity_pos.title.text = "velocity profile at given position, Re: {0}".format(round(Re, 2))
+
+        else:
+            self.source.data = dict(x=r, y=ur, P=p)
+            self.plot_velocity_pos.xaxis.axis_label = "Ur [m/s]"
+            self.plot_velocity_pos.title.text = "velocity profile at given position, Re: {0}".format(round(Re, 2))
+
+        self.source_space.data = dict(x=X, y=R, P=P, U=U, P_tot=P_tot,)
         self.source_pos.data = dict(x=xPos, r=rPos, P=pPos,)
         
         if viscous == "True":
@@ -206,11 +242,29 @@ class SetupApp:
         if relFilePath:
             solutionDict_path = join('stenosis', 'data', 'solutionDict.p')
             solutionDict_space_path = join('stenosis', 'data', 'solutionDict_space.p')
+            solutionDict_path_mu = join('stenosis', 'data', 'solutionDict_mu.p')
+            solutionDict_space_path_mu = join('stenosis', 'data', 'solutionDict_space_mu.p')
+            solutionDict_path_geom_mu = join('stenosis', 'data', 'solutionDict_geom_mu.p')
+            solutionDict_space_path_geom_mu = join('stenosis', 'data', 'solutionDict_space_geom_mu.p')
+            solutionDict_path_geom = join('stenosis', 'data', 'solutionDict_geom.p')
+            solutionDict_space_path_geom = join('stenosis', 'data', 'solutionDict_space_geom.p')
             
         else:
             solutionDict_path = join(dirname(__file__), 'data', 'solutionDict.p')
             solutionDict_space_path = join(dirname(__file__), 'data', 'solutionDict_space.p')
+            solutionDict_path_mu = join(dirname(__file__), 'data', 'solutionDict_mu.p')
+            solutionDict_space_path_mu = join(dirname(__file__), 'data', 'solutionDict_space_mu.p')
+            solutionDict_path_geom_mu = join(dirname(__file__), 'data', 'solutionDict_geom_mu.p')
+            solutionDict_space_path_geom_mu = join(dirname(__file__), 'data', 'solutionDict_space_geom_mu.p')
+            solutionDict_path_geom = join(dirname(__file__), 'data', 'solutionDict_geom.p')
+            solutionDict_space_path_geom = join(dirname(__file__), 'data', 'solutionDict_space_geom.p')
         solutionDict = pickle.load(open(solutionDict_path, "rb"))
         solutionDict_space = pickle.load(open(solutionDict_space_path, "rb"))
+        solutionDict_mu = pickle.load(open(solutionDict_path_mu, "rb"))
+        solutionDict_space_mu = pickle.load(open(solutionDict_space_path_mu, "rb"))
+        solutionDict_geom_mu = pickle.load(open(solutionDict_path_geom_mu, "rb"))
+        solutionDict_space_geom_mu = pickle.load(open(solutionDict_space_path_geom_mu, "rb"))
+        solutionDict_geom = pickle.load(open(solutionDict_path_geom, "rb"))
+        solutionDict_space_geom = pickle.load(open(solutionDict_space_path_geom, "rb"))
         
-        return solutionDict, solutionDict_space
+        return solutionDict, solutionDict_space, solutionDict_mu, solutionDict_space_mu, solutionDict_geom, solutionDict_space_geom, solutionDict_geom_mu, solutionDict_space_geom_mu
